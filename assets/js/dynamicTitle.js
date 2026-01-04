913898f60b8e4ca199fe3e36cc77bdca6c128a8e
Lam Pham <phamla@dickinson.edu>
2026-01-04 01:36:56 -0500

    Update

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -1,20 +1,20 @@
 window.onload = function () {
-  const favicon = document.getElementById('favicon')
-  const pageTitle = document.title
-  const attentionMessage = 'Come back'
+  const favicon = document.getElementById("favicon");
+  const pageTitle = document.title;
+  const attentionMessage = "Come back";
 
-  document.addEventListener('visibilitychange', function (e) {
-    const isPageActive = !document.hidden
-    toggle(isPageActive)
-  })
+  document.addEventListener("visibilitychange", function (e) {
+    const isPageActive = !document.hidden;
+    toggle(isPageActive);
+  });
 
   function toggle(isPageActive) {
     if (isPageActive) {
-      document.title = pageTitle
-      favicon.href = './assets/avatar.jpg'
+      document.title = pageTitle;
+      favicon.href = "./assets/avatar.jpg";
     } else {
-      document.title = attentionMessage
-      favicon.href = './assets/images/folded.png'
+      document.title = attentionMessage;
+      favicon.href = "./assets/images/folded.png";
     }
   }
-}
+};
